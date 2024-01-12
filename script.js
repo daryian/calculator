81ab36978e06cc8abef8230c2770258c96c86d94
Daryian <johnsondaryian@gmail.com>
2024-01-11 18:31:46 -0800

    working on calculations

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let numOne = '';
 let numTwo = '';
 let buttonValue = 0;
-const displayContainer = document.querySelector("#display-container");
+let opValue = '';
+let operator = '';
+let displayContainer = document.querySelector("#display-container");
 const numInput = document.querySelectorAll(".number");
 const clearButton = document.querySelector("#clear");
 const operatorInput = document.querySelectorAll(".operator");
@@ -11,73 +13,97 @@ runCalc();
 
 
 
-function runCalc() {
-    assignNum();
-    clearAll();
-    assignOperators();
-}
+function getNums(){
+    if(opValue != '') {
+        if(numOne === '') {
+            numOne = displayContainer.innerText;
+            operator = opValue;
+            opValue = '';
+            displayContainer.innerText = 0; 
+            console.log("number one =", numOne);
+        }
+        else {
+            numTwo = displayContainer.innerText;
+            console.log("number two =", numTwo);
+            Number(numOne, numTwo);
+            console.log(numOne, operator, numTwo)
+            numOne = operate(numOne, operator,numTwo);
+            displayContainer.innerText = numOne;
+            numTwo = '';
+        }
+        
 
-function clearAll() {
-    clearButton.addEventListener("click", () =>  {
-        displayContainer.innerText = 0;
-        buttonValue = 0;
-    })
-}
 
-function assignOperators() {
-    
 }
-
-function assignNum() {
+}
+function assignButtons() {
     for(let i = 0; i < numInput.length; i++) {
         numInput[i].addEventListener("click", () => {
             buttonValue = numInput[i].innerText;
 
             if(displayContainer.innerText != 0) {
-                displayContainer.innerText +=buttonValue;
+                displayContainer.innerText += buttonValue;
             }
             else{
                 displayContainer.innerText = buttonValue;
             }
         });
     }
+
+    for(let i = 0; i < operatorInput.length; i++) {
+        operatorInput[i].addEventListener("click", () => {
+        opValue = operatorInput[i].textContent;
+        console.log(opValue); 
+        getNums();
+        });
+    }
+    
 }
 function add(numOne, numTwo){
-    let result = numOne + numTwo;
+    let result = parseFloat(numOne) + parseFloat(numTwo);
     return result;
 }
 
 function subtract(numOne, numTwo){
-    let result = numOne - numTwo;
+    let result = parseFloat(numOne) - parseFloat(numTwo);
     return result;
 }
 
 function multiply(numOne, numTwo){
-    let result = numOne * numTwo;
+    let result =parseFloat(numOne) * parseFloat(numTwo);
     return result;
 }
 
 function divide(numOne, numTwo){
-    let result = numOne / numTwo;
+    let result = parseFloat(numOne) / parseFloat(numTwo);
     return result;
 }
 
 function operate(numOne, operator, numTwo){
+    let result = 0;
     switch(operator) {
-        case 'add':
-            add(numOne, numTwo);
-            break;
-    
-        case 'sub':
-            subtract(numOne, numTwo);
-            break;
-
-        case 'mul':
-            multiply(numOne, numTwo);
-            break;
-
-        case 'div':
-            divide(numOne,numTwo);
-            break;
+        case '+':
+            result = add(numOne, numTwo);
+            return result;
+        case '-':
+            result = subtract(numOne, numTwo);
+            return result;
+        case '*':
+            result = multiply(numOne, numTwo);
+            return result;
+        case '/':
+            result = divide(numOne,numTwo);
+            return result;
     }
+}
+
+function clearAll() {
+    clearButton.addEventListener("click", () =>  {
+        displayContainer.innerText = 0;
+        buttonValue = 0;
+        opValue = '';
+    })
+}function runCalc() {
+    clearAll();
+    assignButtons();
 }
